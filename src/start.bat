6c2eff16cf8ae864cde68b45d475407e1072988a
Klemen Kenda <klemen.kenda@ijs.si>
2018-05-09 12:20:43 +0200

    Start procedure for experiments.

---
+++ src/start.bat
@@ -0,0 +1,11 @@
+start cmd /c "python run-energy.py -n id11010024 -t 12"
+start cmd /c "python run-energy.py -n id11010024 -f 13"
+
+start cmd /c "python run-energy.py -n id12041022 -t 12"
+start cmd /c "python run-energy.py -n id12041022 -f 13"
+
+start cmd /c "python run-energy.py -n id12041992 -t 12"
+start cmd /c "python run-energy.py -n id12041992 -f 13"
+
+start cmd /c "python run-energy.py -n id14061721 -t 12"
+start cmd /c "python run-energy.py -n id14061721 -f 13"
